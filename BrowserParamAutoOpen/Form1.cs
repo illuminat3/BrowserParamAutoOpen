fce008b8bea12a527fac11448b03b2b3665f8ec3
Matthew Rawson <matthewrawson2@gmail.com>
2024-11-18 12:11:06 +0000

    Added Textbox

--- BrowserParamAutoOpen/Form1.cs
+++ BrowserParamAutoOpen/Form1.cs
@@ -1,10 +1,26 @@
+using System;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
 namespace BrowserParamAutoOpen
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        public Form1(string[] args)
         {
             InitializeComponent();
+
+            // Check if a locationString is passed
+            if (args.Length > 0)
+            {
+                string locationString = args[0]; // Capture the locationString
+                DisplayLocation(locationString);
+            }
+        }
+
+        private void DisplayLocation(string locationString)
+        {
+            textBox1.Text = locationString;
         }
     }
 }
