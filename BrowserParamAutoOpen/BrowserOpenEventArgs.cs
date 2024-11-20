372eef08589ce59f3cdce5bf7e8a0caa8071266b
Matthew Rawson <matthewrawson2@gmail.com>
2024-11-20 09:39:00 +0000

    Add event args for text from browser

---
+++ BrowserParamAutoOpen/BrowserOpenEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrowserParamAutoOpen
+{
+    public class BrowserOpenEventArgs : EventArgs
+    {
+        public string Args { get; }
+
+        public BrowserOpenEventArgs(string args)
+        {
+            Args = args;
+        }
+    }
+}
