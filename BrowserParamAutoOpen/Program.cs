b45955b68e9171288fbe22ca54e6c3021868b499
Matthew Rawson <matthewrawson2@gmail.com>
2024-11-20 09:42:17 +0000

    Removed MUTEX from program.cs and improved the startup logic

--- BrowserParamAutoOpen/Program.cs
+++ BrowserParamAutoOpen/Program.cs
@@ -1,30 +1,59 @@
-using System.Net.Sockets;
+using System;
+using System.Web;
+using System.Windows.Forms;
 
 namespace BrowserParamAutoOpen
 {
     internal static class Program
     {
-        private static Mutex mutex;
-
         [STAThread]
         static void Main(string[] args)
         {
-            const string mutexName = "BrowserParamAutoOpenMutex";
-            bool isOwned;
+            var processedArgs = string.Join(" ", args);
+            string passthroughText = string.Empty;
 
-            mutex = new Mutex(true, mutexName, out isOwned);
-
-            if (!isOwned)
+            if (!string.IsNullOrEmpty(processedArgs))
             {
-                // An instance is already running; pass arguments and focus it
-                IPCClient.SendArgsToRunningInstance(args);
+                passthroughText = HandleBrowserText(processedArgs);
+
+            }
+
+            if (IsAnotherInstanceRunning(passthroughText))
+            {
                 return;
             }
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1(args));
-            mutex.ReleaseMutex();
+            IPCServer.StartServer();
+
+            Application.Run(new Form1(passthroughText));
+        }
+
+        private static bool IsAnotherInstanceRunning(string args)
+        {
+            try
+            {
+                IPCClient.SendArgsToRunningInstance(args);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private static string HandleBrowserText(string input)
+        {
+            const string prefix = "autoopen://";
+            if (input.StartsWith(prefix, StringComparison.OrdinalIgnoreCase))
+            {
+                input = input.Substring(prefix.Length);
+            }
+
+            string decodedText = HttpUtility.UrlDecode(input);
+
+            return decodedText[..^1];
         }
     }
 }
